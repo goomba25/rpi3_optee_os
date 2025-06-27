6317fd23372fdc41e0b613929bb404686cf340b2
peter <ajtwls0205@naver.com>
2025-06-28 04:43:06 +0900

    [ENV] Add sdk
    - Add sdk for client app
    - Add sdk for trusted app

--- env/Docker/image_build.sh
+++ env/Docker/image_build.sh
@@ -1,6 +1,6 @@
 docker build --force-rm -t optee-image \
 --build-arg USERNAME=`whoami` \
---build-arg PASSWORD=gkstmd25 \
+--build-arg PASSWORD= \
 --build-arg UID=$(id -u) \
 --build-arg GID=$(id -g) \
 .
